088e8c182896075cd18bb0574e53632d1878689a
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-18 15:55:35 +0530

    done some more exercises

--- Exercise - 1/Exercise9.js
+++ Exercise - 1/Exercise9.js
@@ -1,14 +1,38 @@
 // Write a program that takes a string and prints out the number of vowels in the string. 
-function countVowels(str)
-{
-    let count = 0;
-    for (let s of str){
-        if(s === 'a' || s === 'e' || s === 'o' || s === 'i' || s === 'u')
-        {
-            count++;
-        }
-    }
-    return count;
-}
 
-console.log(countVowels("Priyansh"));
+function countVowels(str){
+    count = 0 ;
+    for (let s of str ) {
+        if(s === 'a' || s === 'e' || s === 'i' || s === 'o' || s === 'u'  ){
+            count ++ ; 
+        }
+       
+    }
+    return count ;
+}
+console.log(countVowels("aashish"));
+
+
+
+
+
+
+
+
+
+
+
+
+// function countVowels(str)
+// {
+//     let count = 0;
+//     for (let s of str){
+//         if(s === 'a' || s === 'e' || s === 'o' || s === 'i' || s === 'u')
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countVowels("Priyansh"));
