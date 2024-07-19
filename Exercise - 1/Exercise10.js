d60dacd21c40db81deb4f257965d39892ffac3d4
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-19 22:58:13 +0530

    done some more exercises

--- Exercise - 1/Exercise10.js
+++ Exercise - 1/Exercise10.js
@@ -2,23 +2,59 @@
 // For example, given the arrays [1, 2, 3, 4, 5] and [3, 4, 5, 6, 7], the function should return [3, 4, 5].
 // Hint: You may need to use nested loops and conditional statements to solve this problem.
 
-function findCommonElements(arr1,arr2)
-{
-    let commonElements = [];
-    for(let i = 0; i < arr1.length; i++)
-    {
-        if(arr2.includes(arr1[i]))
-        {
-            if(!commonElements.includes(arr2[i]))
-            {
-                commonElements.push(arr1[i]);
-            }
-        }
-    }
-    return commonElements;
-}
 
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [3, 4, 5, 6, 7];
-const commonElements = findCommonElements(arr1, arr2);
-console.log(commonElements); 
+
+
+function findCommonArry(arr1 , arr2){
+    let commonELementArr = [];
+    for (let i = 0 ; i < arr1.length ; i++)
+    {
+         if (arr2.includes(arr1[i]) )
+         {
+            if(!commonELementArr.includes(arr2[i])){
+                commonELementArr.push(arr1[i])
+            }
+         }
+    }
+    return commonELementArr;
+
+}
+    arr1  = [1 , 2 ,3 , 5 , 9];
+    arr2 =  [1 , 4 , 6, 8 , 10];
+    console.log(findCommonArry(arr1 , arr2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findCommonElements(arr1,arr2)
+// {
+//     let commonElements = [];
+//     for(let i = 0; i < arr1.length; i++)
+//     {
+//         if(arr2.includes(arr1[i]))
+//         {
+//             if(!commonElements.includes(arr2[i]))
+//             {
+//                 commonElements.push(arr1[i]);
+//             }
+//         }
+//     }
+//     return commonElements;
+// }
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = [3, 4, 5, 6, 7];
+// const commonElements = findCommonElements(arr1, arr2);
+// console.log(commonElements); 
--- Exercise - 1/Exercise9.js
+++ Exercise - 1/Exercise9.js
@@ -11,28 +11,3 @@ function countVowels(str){
     return count ;
 }
 console.log(countVowels("aashish"));
-
-
-
-
-
-
-
-
-
-
-
-
-// function countVowels(str)
-// {
-//     let count = 0;
-//     for (let s of str){
-//         if(s === 'a' || s === 'e' || s === 'o' || s === 'i' || s === 'u')
-//         {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
-
-// console.log(countVowels("Priyansh"));
