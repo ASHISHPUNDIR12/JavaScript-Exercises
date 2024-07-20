9e8acdb3e6592fff29a87c67d2dbf877bf63309e
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-20 21:50:13 +0530

    another one

--- Exercise - 2/Exercise2.js
+++ Exercise - 2/Exercise2.js
@@ -1,5 +1,7 @@
 // Write a function called 'getSquare' that takes a number parameter and returns its square.
 
-const getSquare = number => number * number;
-
-console.log(getSquare(4));
+function getsquare(num){
+    let squareOfNum = (num * num)
+    console.log(squareOfNum)
+}
+getsquare(10);
