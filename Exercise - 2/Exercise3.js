80ac2c881f850290d389c2bf31db6c106f19b23d
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-20 22:02:37 +0530

    another one

--- Exercise - 2/Exercise3.js
+++ Exercise - 2/Exercise3.js
@@ -1,19 +1,46 @@
 // Write a function called 'countLetters' that takes a string parameter and returns an object that contains a count of each letter in the string.
 
-function countLetters(str)
-{
+function countLetters(str){
     let count = {};
-    for(let ch of str)
-    {
-        if(count[ch])
-        {
-            count[ch]++;
-        }
-        else{
-            count[ch] = 1;
-        }
+    for(let ch of str){
+      if(count[ch]){
+        count[ch]++
+      }
+      else{
+        count[ch] = 1
+      }
     }
-    return count;
+    return count ;
 }
 
-console.log(countLetters("My Name is Priyansh PAtel"));
+console.log(countLetters("hii i am ashish "));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function countLetters(str)
+// {
+//     let count = {};
+//     for(let ch of str)
+//     {
+//         if(count[ch])
+//         {
+//             count[ch]++;
+//         }
+//         else{
+//             count[ch] = 1;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countLetters("My Name is Priyansh PAtel"));
