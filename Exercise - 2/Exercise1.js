a154f63740a8a657d7f84de51699d7be2c10037b
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-20 21:47:27 +0530

    done some more exercises

--- Exercise - 2/Exercise1.js
+++ Exercise - 2/Exercise1.js
@@ -1,9 +1,8 @@
 // Write a function called 'greet' that takes a name parameter and logs a greeting message to the console. 
 
-function greet(name)
-{
-    console.log(`Hello ${name}`);
+function greet(name){
+    console.log(`Hello ${name} `)
 }
 
 
-greet("Priyansh");
+ greet("ashish")
