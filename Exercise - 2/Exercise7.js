b8d0ab6136d8512cf4b19a6249e9cff8201ebe50
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-21 21:47:15 +0530

    another one

--- Exercise - 2/Exercise7.js
+++ Exercise - 2/Exercise7.js
@@ -1,17 +1,51 @@
 // Write a function that takes an array of strings as an argument and returns a new array with only the strings that have a length greater than 5.
 
-function filterString(strings) {
-    let result = [];
-    for (let str of strings) {
-        if(str.length > 5)
-        {
-            result.push(str);
+function BiggerString(str){
+    let newArray = [];
+    for(let word of str ){
+        if(word.length > 5 ){
+            newArray.push(word);
         }
     }
-    return result;
+    return newArray;
 }
+    const StringArray = ["aashish " , "seenu" , "sanu" , "om"]
+    console.log(BiggerString(StringArray));
 
-const strings = ["Priyansh", "Khushi", "Queen", "King", "Prishi", "Prish"];
 
-console.log(strings);
-console.log(filterString(strings));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function filterString(strings) {
+//     let result = [];
+//     for (let str of strings) {
+//         if(str.length > 5)
+//         {
+//             result.push(str);
+//         }
+//     }
+//     return result;
+// }
+
+// const strings = ["Priyansh", "Khushi", "Queen", "King", "Prishi", "Prish"];
+
+// console.log(strings);
+// console.log(filterString(strings));
