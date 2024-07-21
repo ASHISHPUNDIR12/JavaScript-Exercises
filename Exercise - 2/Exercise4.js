881c688859969551a5ca9c4b4e073f5e9a8e2876
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-21 10:50:23 +0530

    another one

--- Exercise - 2/Exercise4.js
+++ Exercise - 2/Exercise4.js
@@ -1,17 +1,14 @@
 // Write a function called 'createCounter' that returns a function. The returned function should increment a counter variable each time it is called and return the current count.
 
-function createCounter()
-{
-    let counter = 0;
-    return function()
-    {
-        counter++;
-        return counter;
+function createCounter(){
+   let  Counter  = 0;
+    return function (){
+        Counter ++ ; 
+        return Counter ;
     }
 }
-
 const counter = createCounter();
-
-console.log(counter());
-console.log(counter());
-console.log(counter());
+    console.log(counter());
+    console.log(counter());
+    console.log(counter());
+    console.log(counter());
