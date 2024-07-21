adce934cab47248c07e5aeeabb046d5aba8bdcec
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-21 21:12:56 +0530

    another one

--- Exercise - 2/Exercise5.js
+++ Exercise - 2/Exercise5.js
@@ -1,14 +1,18 @@
 // Write a function called 'sumEvenNumbers' that takes an array of numbers as a parameter and returns the sum of all even numbers in the array.
 
-function sumEvenNumbers(numbers) {
-    let sum = 0;
-    for (let number of numbers) {
-        sum += !(number & 1) ? number : 0;
-        // console.log(number)
+
+function sumEvenNumbers(arr){
+    let sum  = 0;
+    for(let s of arr){
+        if(s % 2 == 0)
+            {
+                sum = sum + s
+            }
+            else {
+                sum = sum + 0;
+            }
     }
     return sum;
 }
-
-const array = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-
-console.log(sumEvenNumbers(array));
+ let arrry = [1 ,3 ,2 ,4]
+console.log(sumEvenNumbers(arrry))
