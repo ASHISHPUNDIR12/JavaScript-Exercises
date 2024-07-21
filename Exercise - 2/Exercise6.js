eab7c2bfe331dd7ebbdf4a4c10473c9d5488bebb
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-21 21:36:55 +0530

    another one

--- Exercise - 2/Exercise6.js
+++ Exercise - 2/Exercise6.js
@@ -1,13 +1,12 @@
 // Write a function that takes an array of numbers as an argument and returns the sum of all the numbers in the array.
 
-function sum(numbers) {
-    let sum = 0;
-    for (let number of numbers) {
-        sum += number;
+
+function sumOfArray(arr){
+    let sum =  0;
+    for(let number of arr){
+        sum +=number ; 
     }
-    return sum;
+    return sum ; 
 }
-
-const array = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-
-console.log(sum(array));
+const array = [1 ,2  , 2]
+console.log(sumOfArray(array));
